cb41d22a01698308087e67db05c550bce10e5710
jitendra <jitendranayak182@gmail.com>
2020-08-22 20:20:34 +0530

    Updates

---
+++ precache-manifest.9533bf339eeaffb613d0e1e351ca6078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e45e9b72782e25de148288416b2a823",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "eb8fca70c75c9c10bbed",
+    "url": "/myprofile/static/css/main.b8ba023d.chunk.css"
+  },
+  {
+    "revision": "2c466a754bb6b7d84aa7",
+    "url": "/myprofile/static/js/2.a8c181d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.a8c181d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8fca70c75c9c10bbed",
+    "url": "/myprofile/static/js/main.519d209e.chunk.js"
+  },
+  {
+    "revision": "bfafe7cbdcc7babe7919",
+    "url": "/myprofile/static/js/runtime-main.7af89b8f.js"
+  }
+]);
